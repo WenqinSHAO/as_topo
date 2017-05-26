9bba7fec03c45b18211c797a233c6a07d923a219
Wenqin SHAO <samuelshwq@gmail.com>
2017-05-26 16:01:21 +0200

    add support for congestion graph

--- js_lib/vis.js
+++ js_lib/vis.js
@@ -1,46 +1,25 @@
 // the script is based on https://bl.ocks.org/mbostock/4062045
-//container of svg
-var container = d3.select('body').append('div')
-        .attr('id', 'container');
 
-var svg = container.append("svg")
-        .attr('id', 'graph')
-        .attr('width', 5000)
-        .attr('height', 5000);
+var container = d3.select('div#container');
+
+var svg = container.select("svg#graph");
+
+svg.attr('width', 5000)
+   .attr('height', 5000);
 
 var width = svg.attr('width');
 var height = svg.attr('height');
 
-// button to choose local files
-// TODO: http://jsfiddle.net/u2kJq/241/
-// right click to load file could be better
-var button = d3.select('body').append("input")
-        .attr("type", "file")
-        .attr("accept", ".json")
-        .style("margin", "5px")
-        .on("change", function() {
-            var file = d3.event.target.files[0];
-            if (file) {
-                var reader = new FileReader();
-                reader.onloadend = function(evt) {
-                    var data;
-                    try {
-                        data = JSON.parse(evt.target.result);
-                    } catch(ex) {
-                        console.error(ex);
-                    }
-                    // clean the svg
-                    svg.selectAll("*").remove();
-                    // handle new data from file
-                    plot(data);
-                };
-                reader.readAsText(file);
-            }
-        });
-
 // text box for graph info
-var graphinfo = d3.select("body").append("pre")
-        .attr('id', 'graphinfo');
+var graphinfo = d3.select("pre#graphinfo")
+
+var linktip = d3.select("body").append("div")
+                    .attr("class", "linktip")
+                    .style("opacity", 0);
+
+var nodetip = d3.select("body").append("div")
+                    .attr("class", "nodetip")
+                    .style("opacity", 0);
 
 // node color legend:
 // 1 for source; 2 for ixp; 3 for dst; 4 for all the others
@@ -48,10 +27,75 @@ var color = d3.scaleOrdinal()
         .domain([1,2,3,4])
         .range(["#bebada", "#fdb462", "#e41a1c", "#b3de69"]);
 
+var tparser = d3.utcParse("%Y-%m-%d %H:%M");
+var tformatter = d3.utcFormat("%Y-%m-%d %H:%M");
 
+var opened_f, loaded_data, moment, is_congestion_graph, bin_size, begin, end;
 
+function plot() {
+    var file = document.getElementById("file_input");
+    if ('files' in file && file.files.length > 0) {
+        var f = file.files[0]; // get the first file selected by the file input
+        if (f) {
+            if (f != opened_f) {
+                // only proceed if it differs from the file already opened
+                graphinfo.text("Reading data file...");
+                var reader = new FileReader();
+                reader.onloadend = function(evt) {
+                    //var t1 = performance.now();
+                    //d3.select("#status").text(Math.round(f.size/(1048576)) + "MB loaded in " + parseFloat(Math.round((t1-t0) * 100) / 100).toFixed(2) + " milliseconds.\n Now scoping and plotting data...");
+                    //console.log(f.name + ": " + Math.round(f.size/(1048576)) + "MB, " + parseFloat(Math.round((t1-t0) * 100) / 100).toFixed(2) + "msec");
+                    opened_f = f;
+                    try {
+                        loaded_data = JSON.parse(evt.target.result); // read the file as text and parse it to JSON object
+                    } catch (ex) {
+                        console.error(ex);
+                    }
+                    // cannot merge with else clause: see http://stackoverflow.com/questions/13487437/change-global-variable-inside-javascript-closure
+                    // event driven not sequential
+                    svg.selectAll("*").remove();
+                    init();
+                };
+                //var t0 = performance.now();
+                reader.readAsText(f);
+            } else {
+                //d3.select("#status").text("Scoping and plotting data...");
+                //svg.selectAll("*").remove(); // clean the canvas
+                update();
+            }
+        }
+    }
+}
 
-var plot = function(graph) {
+function init() {
+      if (loaded_data.hasOwnProperty("congestion")) {
+        is_congestion_graph = loaded_data.congestion;
+        //console.log("is congestion graph " + is_congestion_graph);
+      } else {
+        is_congestion_graph = false
+      }
+
+      if (is_congestion_graph) {
+          if (loaded_data.graph.hasOwnProperty("cpt_bin_size")) {
+            bin_size = loaded_data.graph.cpt_bin_size;
+            //console.log("bin size " + bin_size);
+          }
+
+          if (loaded_data.graph.hasOwnProperty("congestion_begin")) {
+            begin = loaded_data.graph.congestion_begin * 1000;
+            //console.log(tformatter(begin));
+          }
+
+          if (loaded_data.graph.hasOwnProperty("congestion_end")) {
+            end = loaded_data.graph.congestion_end * 1000;
+            //console.log(tformatter(end));
+          }
+
+          if(! tparser(document.getElementById("datetime").value)) {
+            moment = Math.floor(loaded_data.graph.congestion_begin/bin_size) * bin_size * 1000;
+            document.getElementById("datetime").value = tformatter(moment);
+          }
+      }
 
       var simulation = d3.forceSimulation()
             .force("link", d3.forceLink().id(function(d) { return d.id; }))
@@ -75,53 +119,76 @@ var plot = function(graph) {
           d.fy = null;
       }
 
-      d3.select("#graphinfo").text(JSON.stringify(graph.graph, undefined, 2));
+      d3.select("#graphinfo").text(JSON.stringify(loaded_data.graph, undefined, 2));
 
       // add links
       var link = svg.append("g")
             .attr("class", "links")
         .selectAll("line")
-        .data(graph.links)
+        .data(loaded_data.links)
         .enter().append("line")
+            .attr("congestion_level", congestion)
+            .attr("stroke", linkColor)
             .attr("stroke-width", function(d) { return 2 * Math.sqrt(d.probe.length); })
-            .attr("opacity", function(d) { return (d.probe.length > 30) ? 0.6: 0.1; } )
-            .on("dblclick", saveLink);
+            .attr("opacity", linkOpacity)
+            .on("dblclick", saveLink)
+            .on("mouseover", function(d) {
+                linktip.transition()
+                    .duration(200)
+                    .style("opacity", .7);
+                var text = d.probe.length.toString() + " probes on (" + d.src_name+ ", " + d.tgt_name + ")";
+                if (is_congestion_graph) {
+                    text += '<br>' + 'congestion level: ' + d3.select(this).attr("congestion_level");
+                }
+                linktip.html(text)
+                    .style("left", (d3.event.pageX - 80) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                linktip.transition()
+                    .duration(200)
+                    .style("opacity", 0);
+            });
 
       // add nodes
       var node = svg.append("g")
             .attr("class", "nodes")
         .selectAll("circle")
-        .data(graph.nodes)
+        .data(loaded_data.nodes)
         .enter().append("circle")
             .attr("r", 6)
             .attr("fill", nodeColor)
             .call(d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended))
-            .on("click", showPath);
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended)
+                )
+            .on("click", showPath)
+            .on("mouseover", function (d) {
+                nodetip.transition()
+                    .duration(200)
+                    .style("opacity", .7);
+                var text = d.name;
+                if (d.hasOwnProperty('hosting')) {
+                    text += "<br> hosting: " + d.hosting.toString();
+                }
+                nodetip.html(text)
+                    .style("left", (d3.event.pageX - 80) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                nodetip.transition()
+                    .duration(200)
+                    .style("opacity", 0);
+            });
 
-      // node title and link title
-      node.append("title")
-          .text(function(d) {
-              if (_.has(d, 'hosting')) {
-                  return 'AS '+d.name + ' hosting:\n' + d.hosting.toString();
-                  }
-              else {
-                  return 'AS '+d.name;
-                  }
-           });
-
-      link.append("title")
-          .text(function(d) {
-              return d.probe.length.toString() + ' probes on (' + d.src_name+ ', ' + d.tgt_name + ')\n' + '[' + d.probe.toString() + ']'; });
 
       simulation
-          .nodes(graph.nodes)
+          .nodes(loaded_data.nodes)
           .on("tick", ticked);
 
       simulation.force("link")
-          .links(graph.links);
+          .links(loaded_data.links);
 
       function ticked() {
             link
@@ -137,19 +204,19 @@ var plot = function(graph) {
 }
 
 // what happens once a node is clicked
-function showPath(d, i ) {
+function showPath(d) {
     if (!d3.select(this).classed('show-path')) {
         if (_.has(d, 'hosting')) {
             d3.select(this).attr("fill", "#4a1486");
             var pb = d.hosting;
             d3.selectAll('line').each(function (d) {
-            for (var i = 0; i < pb.length; i++) {
-                if (d.probe.includes(pb[i])){
-                    d3.select(this)
-                      .style("stroke", "#e34a33")
-                      .attr("opacity", .3)
-                      .attr("stroke-width", function(d) { return 6 * Math.sqrt(d.probe.length); });
-                    break;
+                for (var i = 0; i < pb.length; i++) {
+                    if (d.probe.includes(pb[i])){
+                        d3.select(this)
+                            .style("stroke", linkColorSelected)
+                            .attr("opacity", .3)
+                            .attr("stroke-width", function(d) { return 6 * Math.sqrt(d.probe.length); });
+                        break;
                     }
                 }
             });
@@ -163,9 +230,9 @@ function showPath(d, i ) {
                 for (var i = 0; i < pb.length; i++) {
                     if (d.probe.includes(pb[i])){
                         d3.select(this)
-                            .style("stroke", "#999")
+                            .style("stroke", linkColor)
                             .attr("stroke-width", function(d) { return 2 * Math.sqrt(d.probe.length); })
-                            .attr("opacity", function(d) { return (d.probe.length > 30) ? 0.6: 0.1; } );
+                            .attr("opacity", linkOpacity);
                         break;
                     }
                 }
@@ -175,6 +242,26 @@ function showPath(d, i ) {
     }
 }
 
+function update() {
+    if (is_congestion_graph) {
+        var m =  tparser(document.getElementById("datetime").value);
+        m = m ? m : begin;
+        moment = Math.floor(m / (bin_size * 1000)) * bin_size * 1000;
+        document.getElementById("datetime").value = tformatter(moment);
+        svg.selectAll("line")
+            .attr("congestion_level", congestion)
+            .attr("stroke", linkColor)
+            .attr("opacity", linkOpacity);
+    } else {
+        alert("Only congestion graph can be navigated in time.");
+    }
+}
+
+function navigator(is_forward) {
+    var m = is_forward? moment + bin_size * 1000 : moment - bin_size * 1000;
+    document.getElementById("datetime").value = tformatter(m);
+    update();
+}
 
 // what happens if a link is double clicked
 function saveLink(d) {
@@ -195,5 +282,104 @@ function nodeColor(d) {
         return color(4);
     }
 }
-// load example graph at the beginning
-//plot("example.json")
+
+function congestion(d) {
+    if(is_congestion_graph){
+        return datetimeSearch(d.congestion, moment);
+    } else {
+        return 'NA';
+    }
+}
+
+function linkOpacity(d) {
+    if (is_congestion_graph) {
+        var level = d3.select(this).attr("congestion_level");
+        if (level && level >.1) {
+            return .6;
+        } else {
+            return .1;
+        }
+    } else {
+        return (d.probe.length > 30) ? 0.6: 0.1;
+    }
+}
+
+function linkColor(d) {
+    if(is_congestion_graph){
+        var level = d3.select(this).attr("congestion_level");
+        if (level && level > .1) {
+            return d3.interpolateReds(level);
+        } else {
+        return "#999"
+        }
+    } else {
+        return "#999";
+    }
+}
+
+
+function linkColorSelected(d) {
+    if(is_congestion_graph){
+        var level = d3.select(this).attr("congestion_level");
+        if (level && level > .1) {
+            return d3.interpolateRdPu(level);
+        } else {
+            return "#999"
+        }
+    } else {
+        return "#e34a31";
+    }
+}
+
+function datetimeSearch(arr, v) {
+    // binary search that return arr[i].value with arr[i].epoch == v
+    var low = 0;
+    var high = arr.length;
+    var mid;
+    while (low < high) {
+        mid = Math.floor((low+high)/2);
+        if (arr[mid].epoch * 1000 == v) {
+            return arr[mid].value
+        }
+        if ((arr[mid].epoch * 1000) < v) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return null;
+}
+
+d3.select('body')
+    .on("keydown", function (){
+        if (d3.event.shiftKey) {
+            switch(d3.event.keyCode) {
+                case 39:
+                    navigator(true);
+                    break;
+                case 37:
+                    navigator(false);
+                    break;
+                default:
+                    break;
+            }
+        } else if (d3.event.keyCode == 13){
+            plot();
+        }
+    });
+
+d3.select("input#previous")
+    .on("click", function() {
+        d3.event.stopPropagation();
+        d3.event.preventDefault();
+        navigator(false);
+        return false;
+    });
+
+d3.select("input#next")
+    .on("click", function() {
+        d3.event.stopPropagation();
+        d3.event.preventDefault();
+        navigator(true);
+        return false;
+    });
